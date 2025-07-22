1a119be1d09834971938ead5b3edd5da8439828d
Philipp Heinrich <g260190@l40048.lvt.dkrz.de>
2025-07-22 17:10:36 +0200

    added bash script

---
+++ calc_CF.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=CF
+#SBATCH --output=CF.o%J
+#SBATCH --error=CF.o%J
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=philipp.heinrich@hereon.de
+#SBATCH --mail-type=END,FAIL
+#SBATCH --account=bb1203
+
+srun python3 main.py
