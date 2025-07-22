8f08a73115655d58dd7cebbb71b2870efcaaa40a
Heinricp <philipp.heinrich@hereon.de>
2025-07-22 16:19:42 +0200

    moved functions

--- calculate_CF.py
+++ calculate_CF.py
@@ -1,11 +1,7 @@
 import json
 
+import calc_pv
 import calc_wind
-
-
-def generate_filename(folder: str, variable: str) -> str:
-    parts = folder.split("/")
-    return "_".join([parts[4], parts[6], parts[7], parts[9], variable])
 
 
 def load_folder_locations() -> dict:
@@ -13,12 +9,9 @@ def load_folder_locations() -> dict:
         return json.load(file)
 
 
-def calculate_cf_pv(folder: str, overwrite_existing: bool) -> None:
-    pass
-
-
 def calculate_capacity_factors(overwrite_existing: bool = False):
     nukleus_folders = load_folder_locations()
     for folder_dict in nukleus_folders:
         calc_wind.cf_wind(folder_dict, overwrite_existing)
-        calculate_cf_pv(folder_dict, overwrite_existing)
+        calc_pv.calculate_pv(folder_dict, overwrite_existing)
+        exit()
