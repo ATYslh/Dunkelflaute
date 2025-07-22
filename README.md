c3d4b020d76e2128047ea16c226a5d99ee9e45e6
Heinricp <philipp.heinrich@hereon.de>
2025-07-22 08:48:20 +0200

    adding script to find nukleus data

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Dunkelflaute
+Plan for 
+
+1. Traverse all folders down to the 1hr
+2. Check if rsds and the 100 meter winds are there; otherwise give warning
+3. check if capacity factors have already been calced
+4. create_names for output files (something that contains the gcm,rcm, scenario and all the other jazz)
+5. cut field into correct shape (depending on if EUR-11 or CEU-3)
+6. Create wind
+7. Apply powercurve to wind
+8. Figure out when below 20% threshold
+9. Apply PV to rsds
+10. Apply 20% threshold
+11. use logical_and to figure out when both are below threshold.
+12. If file size is small enough: save files
+13. Apply timmean_mean to create final output
+
+Create wind histograms:
+1. Create histogram for one examplatory hist data
+2. fit curve to it for easier visibility
+3. use the same bins for the same data again
---
+++ find_data.py
@@ -0,0 +1,52 @@
+import os
+
+import yaml
+
+
+def find_directories(root_dir:str, frequency:str):
+    results = []
+    for dirpath, _, _ in os.walk(root_dir):
+        if frequency in dirpath:
+            if {"sfcWind", "rsds"}.issubset(os.listdir(dirpath)):
+                results.append(dirpath)
+    return results
+
+def go_to_version_folder(base_dir:str):
+    with os.scandir(base_dir) as entries:
+        for entry in entries:
+            if entry.is_dir():
+                next_dir = entry.path
+                print("Next directory:", next_dir)
+                exit()
+
+
+def find_nukleus_files(base_directory = "/work/bb1203/data_NUKLEUS_CMOR/"):
+    yaml_file = "nukleus_files.yaml"
+
+    if os.path.exists(yaml_file):
+        os.remove(yaml_file)
+
+    for directory in find_directories(root_dir=base_directory, frequency="1hr"):
+        if "evaluation" in directory:
+            continue
+
+        new_data = {
+            f"{directory}": {
+                "rsds": f"{go_to_version_folder(os.path.join(directory, 'rsds'))}",
+                "sfcWind": f"{go_to_version_folder(os.path.join(directory, 'sfcWind'))}",
+            }
+        }
+
+        # Load existing data if the file exists
+        if os.path.exists(yaml_file):
+            with open(yaml_file, "r") as file:
+                existing_data = yaml.safe_load(file) or {}  # Handle empty file
+        else:
+            existing_data = {}
+
+        # Merge the new data with existing data
+        existing_data.update(new_data)
+
+        # Write the updated data back to the YAML file
+        with open(yaml_file, "w") as file:
+            yaml.dump(existing_data, file, default_flow_style=False, sort_keys=True)
