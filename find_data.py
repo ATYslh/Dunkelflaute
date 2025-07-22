b99135cb91078581c603194881fba1f4fc63c976
Philipp Heinrich <g260190@l40048.lvt.dkrz.de>
2025-07-22 14:15:17 +0200

    rewrote find_data to use json instead of yaml

--- find_data.py
+++ find_data.py
@@ -1,6 +1,5 @@
 import os
-
-import yaml
+import json
 
 
 def find_directories(root_dir: str, frequency: str):
@@ -9,8 +8,7 @@ def find_directories(root_dir: str, frequency: str):
 
     for dirpath, dirnames, filenames in os.walk(root_dir):
         # If this directory matches the desired frequency and has both files, record it
-        if (frequency in dirpath and
-                {"sfcWind", "rsds"}.issubset(dirnames)):
+        if frequency in dirpath and {"sfcWind", "rsds"}.issubset(dirnames):
             results.append(dirpath)
 
         # If we've hit any frequency folder, don't recurse further
@@ -29,25 +27,25 @@ def go_to_version_folder(base_dir: str) -> str | None:
 
 
 def find_nukleus_files(base_directory: str = "/work/bb1203/data_NUKLEUS_CMOR/") -> None:
-    yaml_file = "nukleus_files.yaml"
-    yaml_entries = {}
+    json_file = "nukleus_files.json"
+    json_entries = {}
     for directory in find_directories(root_dir=base_directory, frequency="1hr"):
         if "evaluation" in directory:
             continue
 
-        rsds_folder = go_to_version_folder(os.path.join(directory, 'rsds'))
-        sfcWind_folder = go_to_version_folder(os.path.join(directory, 'sfcWind'))
+        rsds_folder = go_to_version_folder(os.path.join(directory, "rsds"))
+        sfcWind_folder = go_to_version_folder(os.path.join(directory, "sfcWind"))
         if not rsds_folder or not sfcWind_folder:
             continue
 
-        yaml_entries[f"{directory}"] = {
-            "rsds": f"{rsds_folder}",
-            "sfcWind": f"{sfcWind_folder}",
+        json_entries[directory] = {
+            "rsds": rsds_folder,
+            "sfcWind": sfcWind_folder,
         }
 
-    if yaml_entries:
-        with open(yaml_file, "w") as file:
-            yaml.dump(yaml_entries, file, default_flow_style=True, sort_keys=True)
+    if json_entries:
+        with open(json_file, "w") as file:
+            json.dump(json_entries, file, indent=4)
 
 
 if __name__ == "__main__":
