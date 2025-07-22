44061e766ef8b2e4c8b674e60876017cb760013b
Heinricp <philipp.heinrich@hereon.de>
2025-07-22 16:00:44 +0200

    added more content to calculate_CF.py

--- calculate_CF.py
+++ calculate_CF.py
@@ -1,9 +1,24 @@
 import json
 
-def generate_filename(folder:str)->str:
-    parts = folder.split("/")
-    return "_".join([parts[4], parts[6], parts[7], parts[9], parts[-2]])
+import calc_wind
 
-def calculate_capacity_factors():
+
+def generate_filename(folder: str, variable: str) -> str:
+    parts = folder.split("/")
+    return "_".join([parts[4], parts[6], parts[7], parts[9], variable])
+
+
+def load_folder_locations() -> dict:
     with open('nukleus_files.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
+        return json.load(file)
+
+
+def calculate_cf_pv(folder: str, overwrite_existing: bool) -> None:
+    pass
+
+
+def calculate_capacity_factors(overwrite_existing: bool = False):
+    nukleus_folders = load_folder_locations()
+    for folder_dict in nukleus_folders:
+        calc_wind.cf_wind(folder_dict, overwrite_existing)
+        calculate_cf_pv(folder_dict, overwrite_existing)
