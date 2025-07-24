ecdfd0a79d5adb092dbc5cf607a120dd0feca1fc
Philipp Heinrich <g260190@l40049.lvt.dkrz.de>
2025-07-24 16:05:50 +0200

    added more timestamps to main

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ if __name__ == "__main__":
     )
 
     for folder_dict in nukleus_folders:
-        print(f"Start {folder_dict} at {datetime.datetime.now()}", file=sys.stderr)
+        print(f"Start Wind {folder_dict} at {datetime.datetime.now()}", file=sys.stderr)
         cf_wind = calc_wind.cf_wind(nukleus_folders[folder_dict], overwrite_existing)
+        print(f"Start PV {folder_dict} at {datetime.datetime.now()}", file=sys.stderr)
         cf_pv = calc_pv.calculate_pv_main(
             nukleus_folders[folder_dict], overwrite_existing
         )
+        print(f"Start Dunkelflaute {folder_dict} at {datetime.datetime.now()}", file=sys.stderr)
         calc_dunkelflaute.calculate_dunkelflaute(cf_wind, cf_pv, overwrite_existing)
