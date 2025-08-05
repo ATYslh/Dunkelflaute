bf78ba85428fc26bc849089a17115a30389877c9
Philipp <philipp.heinrich@hereon.de>
2025-08-05 16:45:16 +0200

    removed unused file

--- Data_Scripts/Bash_region.sh
+++ Data_Scripts/Bash_region.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=Regional
 #SBATCH --output=regional.o%J
 #SBATCH --error=regional.o%J
-#SBATCH --partition=shared
+#SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=128G
-#SBATCH --cpus-per-task=5
-#SBATCH --time=72:00:00
+#SBATCH --cpus-per-task=38
+#SBATCH --time=08:00:00
 #SBATCH --mail-user=philipp.heinrich@hereon.de
 #SBATCH --mail-type=END,FAIL
 #SBATCH --account=bb1203
