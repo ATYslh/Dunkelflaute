93c99f7be0a8f23f6a3cb50435029805bbfa2a09
Heinricp <philipp.heinrich@hereon.de>
2025-08-05 15:18:44 +0200

    regional_data.py formatting improvements

--- Data_Scripts/regional_data.py
+++ Data_Scripts/regional_data.py
@@ -1,9 +1,12 @@
+import datetime
+import os
+import sys
+from multiprocessing import Pool
+
 import numpy as np
 import xarray as xr
+
 import helper_functions as hpf
-import os
-import datetime
-from multiprocessing import Pool
 
 
 def region_indices(path: str):
@@ -22,8 +25,8 @@ def region_indices(path: str):
             return "127,151,101,143"
         if "WAKOS" in path:
             return "40,60,216,239"
-
-    if "EUR-11" in path:
+        raise ValueError("Cannot determine region region_indices()")
+    elif "EUR-11" in path:
         if "Duisburg":
             return "5,7,39,41"
         if "Germany":
@@ -38,6 +41,10 @@ def region_indices(path: str):
             return "33,38,26,35"
         if "WAKOS":
             return "11,15,55,60"
+        raise ValueError("Cannot determine region region_indices()")
+
+    else:
+        raise ValueError("Cannot determine resolution in region_indices()")
 
 
 def bounding_box(arr) -> tuple[int, int, int, int] | None:
@@ -56,21 +63,21 @@ def bounding_box(arr) -> tuple[int, int, int, int] | None:
 
 def write_selindexboxes():
     mask_path = "/work/bb1203/g260190_heinrich/Dunkelflaute/Subregion_Masks/CEU-3/"
-    CEU3_MASKS = hpf.get_sorted_nc_files(mask_path)
-    for mask in CEU3_MASKS:
+    ceu3_masks = hpf.get_sorted_nc_files(mask_path)
+    for mask in ceu3_masks:
         with xr.open_dataset(mask) as df:
             y_min, x_min, y_max, x_max = bounding_box(df.MASK.values)
-            print(f"if region == '{os.path.basename(mask).split("_")[0]}':")
+            print(f"if region == '{os.path.basename(mask).split('_')[0]}':")
             print(f"    return '{x_min+1},{x_max+1},{y_min+1},{y_max+1}'")
     print("-" * 30)
 
     mask_path = "/work/bb1203/g260190_heinrich/Dunkelflaute/Subregion_Masks/EUR-11/"
-    EUR11_MASKS = hpf.get_sorted_nc_files(mask_path)
-    for mask in EUR11_MASKS:
+    eur11_masks = hpf.get_sorted_nc_files(mask_path)
+    for mask in eur11_masks:
 
         with xr.open_dataset(mask) as df:
             y_min, x_min, y_max, x_max = bounding_box(df.MASK.values)
-            print(f"if region == '{os.path.basename(mask).split("_")[0]}':")
+            print(f"if region == '{os.path.basename(mask).split('_')[0]}':")
             print(f"    return '{x_min+1},{x_max+1},{y_min+1},{y_max+1}'")
 
 
@@ -81,18 +88,17 @@ def crop_masks():
     hpf.run_shell_command(
         "cp -r /work/gg0302/g260190/rsds_analysis/Subregion_Masks/ .", 1
     )
-    CEU_3_mask_files = hpf.get_sorted_nc_files(
+    ceu_3_mask_files = hpf.get_sorted_nc_files(
         "/work/bb1203/g260190_heinrich/Dunkelflaute/Subregion_Masks/CEU-3"
     )
-    EUR_11_mask_files = hpf.get_sorted_nc_files(
+    eur_11_mask_files = hpf.get_sorted_nc_files(
         "/work/bb1203/g260190_heinrich/Dunkelflaute/Subregion_Masks/EUR-11"
     )
-    mask_files = CEU_3_mask_files + EUR_11_mask_files
+    mask_files = ceu_3_mask_files + eur_11_mask_files
     dummy_file = "/scratch/g/g260190/dummy.nc"
     for mask in mask_files:
-        indexbox = hpf.get_indexbox(mask, "cdo")
         hpf.run_shell_command(
-            f"cdo -selindexbox,{region_indices(mask)} -selindexbox,{hpf.get_indexbox(mask,"cdo")} {mask} {dummy_file}",
+            f"cdo -selindexbox,{region_indices(mask)} -selindexbox,{hpf.get_indexbox(mask,'cdo')} {mask} {dummy_file}",
             1,
         )
         hpf.run_shell_command(f"mv {dummy_file} {mask}", 1)
@@ -157,7 +163,7 @@ def process_task(task):
     mask = get_mask(src_file, region)
     sel_idx = region_indices(mask)
     cmd = f"cdo -z zip -ifthen {mask} " f"-selindexbox,{sel_idx} {src_file} {dest_file}"
-    hpf.run_shell_command(cmd, timeout=60)
+    hpf.run_shell_command(cmd, 60)
 
     return None
 
@@ -167,25 +173,26 @@ def create_regional_files():
     regions = ["Duisburg", "IAWAK-EE", "ISAP", "KARE", "KlimaKonform", "WAKOS"]
     subfolders = ["CF_PV", "CF_Wind/3_3MW", "CF_Wind/5MW", "Dunkelflaute", "Wind"]
     num_procs = hpf.process_input_args()
+    total_batches = len(regions) * len(subfolders)
     index = 1
     # open one pool for all subfolders
     with Pool(processes=num_procs) as pool:
         for region in regions:
             for subfolder in subfolders:
-                f"[{index}/30] Starting {region} {subfolder} at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}"
+                print(f"[{index}/{total_batches}] Starting {region} {subfolder} "
+                      f"at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",file=sys.stderr)
                 # gather files for this subfolder
                 germany_dir = os.path.join(base_dir, "Germany", subfolder)
                 files = hpf.get_sorted_nc_files(germany_dir)
-                total = len(files)
 
                 # build the small batch of tasks
                 tasks = [
-                    (region, subfolder, fpath, idx + 1, total)
-                    for idx, fpath in enumerate(files)
+                    (region, subfolder, fpath)
+                    for fpath in files
                 ]
 
                 # dispatch them immediately
-                for idx, filepath, status in pool.imap_unordered(
+                for _ in pool.imap_unordered(
                     process_task, tasks, chunksize=1
                 ):
                     # you can log, collect stats, etc. per file here
