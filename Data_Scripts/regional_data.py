61b886deb5f96449e1ae46333872c6184dbda264
Philipp <philipp.heinrich@hereon.de>
2025-08-07 15:49:05 +0200

    regional_data.py now checks for rsds and tas

--- Data_Scripts/regional_data.py
+++ Data_Scripts/regional_data.py
@@ -118,7 +118,7 @@ def create_folders():
     ]:
         hpf.run_shell_command(f"mkdir {region}", 1)
         hpf.run_shell_command(
-            f"mkdir {region}/CF_PV {region}/CF_Wind {region}/Dunkelflaute {region}/Wind {region}/CF_Wind/3_3MW {region}/CF_Wind/5MW",
+            f"mkdir {region}/CF_PV {region}/tas {region}/rsds {region}/CF_Wind {region}/Dunkelflaute {region}/Wind {region}/CF_Wind/3_3MW {region}/CF_Wind/5MW",
             1,
         )
 
@@ -172,7 +172,7 @@ def process_task(task):
 def create_regional_files():
     base_dir = "/work/bb1203/g260190_heinrich/Dunkelflaute/Data"
     regions = ["Duisburg", "IAWAK-EE", "ISAP", "KARE", "KlimaKonform", "WAKOS"]
-    subfolders = ["CF_PV", "CF_Wind/3_3MW", "CF_Wind/5MW", "Dunkelflaute", "Wind"]
+    subfolders = ["CF_PV", "CF_Wind/3_3MW", "CF_Wind/5MW", "Dunkelflaute", "Wind", "tas", "rsds"]
     num_procs = hpf.process_input_args()
     total_batches = len(regions) * len(subfolders)
     index = 1
