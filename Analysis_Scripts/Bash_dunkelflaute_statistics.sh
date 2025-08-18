1f1029abde7753f67bd08fbb6558b6e963d2f1e3
Philipp <philipp.heinrich@hereon.de>
2025-08-18 14:33:18 +0200

    bash dunkelflaute multithread

--- Analysis_Scripts/Bash_dunkelflaute_statistics.sh
+++ Analysis_Scripts/Bash_dunkelflaute_statistics.sh
@@ -5,11 +5,11 @@
 #SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=256G
-#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=36
 #SBATCH --time=08:00:00
 #SBATCH --mail-user=philipp.heinrich@hereon.de
 #SBATCH --mail-type=END,FAIL
 #SBATCH --account=bb1203
 
-srun python3 Dunkelflaute_statistics.py
+srun python3 Dunkelflaute_statistics.py -c ${SLURM_CPUS_PER_TASK}
